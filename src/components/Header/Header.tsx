35c74f50da3ae34498754c311dcbb134d06dd067
Maksimushka <m.billsrc@gmail.com>
2021-03-15 01:06:36 +0700

    Refactor header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,25 +1,27 @@
 import React from 'react';
 import s from './Header.module.css';
+import styleContainer from './../../common/container.module.css'
+import userPhoto from '../../assets/img/user.png';
 import {NavLink} from "react-router-dom";
 
 const Header = (props: any) => {
     return (
         <header className={s.header}>
-            <div className={s.header_top}>
+            <div className={styleContainer.container}>
                 <NavLink to='/profile' className={s.a}>
-                    <img className={s.header__image}  src='https://www.amic.ru/project/all/architecture-barnaul/images/tild3630-6134-4437-b138-623638326363__medicallogopng32.jpeg' alt='logo' />
-                </NavLink>
-                <div className={s.loginBlock}>
-                    {props.isAuth
-                        ? <div className={s.user}>
-                            <img className={s.userPhoto} src={props.photo} alt="userPhoto"/>
-                            <span className={s.userLogin}>{props.login}</span>
-                        </div>
-                        : <NavLink to={'/login'}>Login</NavLink>
-                    }
+                        <img className={s.header__image}  src='https://www.amic.ru/project/all/architecture-barnaul/images/tild3630-6134-4437-b138-623638326363__medicallogopng32.jpeg' alt='logo' />
+                    </NavLink>
+                <div className={s.user}>
+                        {props.isAuth
+                            ?   <>
+                                    <span className={s.userLogin}>{props.login}</span>
+                                    <img className={s.userPhoto} src={props.photo !== null ? props.photo : userPhoto} alt="userPhoto"/>
+                                </>
+                            : <NavLink to={'/login'}>Login</NavLink>
+                        }
 
+                    </div>
                 </div>
-            </div>
         </header>
     )
 }
